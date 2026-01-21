649603c88d4d47b3cbd95fa9fbdefcf4e5958b47
Aleksy37 <aleksyveillette@gmail.com>
2026-01-21 14:54:44 -0500

    added recursive helper to generate multiple pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,6 @@
 import os
 from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -26,3 +27,15 @@ def generate_page(from_path, template_path, dest_path):
             os.makedirs(dir_path)
     with open(dest_path, "w") as f:
         f.write(page)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry)
+        if os.path.isfile(from_path) and entry.endswith(".md"):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import os
 import shutil
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 
 
 
 
 static_path = "./static"
 public_path = "./public"
+content_path = "./content"
+
 
 def main():
     if os.path.exists(public_path):
         shutil.rmtree(public_path)
     copy_files_recursive(static_path, public_path)
-    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
+    generate_pages_recursive(dir_path_content=content_path, template_path="./template.html", dest_dir_path=public_path)
 
 main()
 
